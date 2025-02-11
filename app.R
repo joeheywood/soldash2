62b84c3a5556346f92fb9fdec7b8450d92e284a0
Joe Heywood <joe.heywood@gmail.com>
2025-02-11 16:30:24 +0000

    more files

---
+++ app.R
@@ -0,0 +1,94 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(robservable)
+library(glue)
+
+source("R/get_chart_data.R")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("State of London Dashboard"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("chapt", 
+                        "Chapter", 
+                        choices = get_chapters()),
+            selectInput("section", 
+                        "..",
+                        choices = list()
+                        ),
+            selectInput("chart",
+                        "..",
+                        choices = list())
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            h3(textOutput("chart_title", )),
+            h4(textOutput("chart_subtitle", )),
+            robservableOutput("main_chart", width = 600),
+            p(uiOutput("src")),
+            h3("dat"),
+            textOutput("test"),
+            tableOutput("tbl")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+    
+    output$chart_title <- renderText({"..."})
+    output$chart_subtitle <- renderText({"..."})
+    
+    output$main_chart <- renderRobservable({
+        chrt <- input$chart
+        print(chrt)
+        if(nchar(chrt) > 0){
+            x <- get_obs_chart(chrt)
+            output$chart_title <- renderText({x$m$title})
+            output$chart_subtitle <- renderText({x$m$sub})
+            output$src <- renderUI({ a(x$m$source, href = x$m$link) })
+            return(x$chart)
+        } else {
+            return(NULL)
+        }
+        
+    })
+    
+    observe({
+        chpt <- input$chapt
+        sects <- get_subsections(chpt)
+        updateSelectInput(session, "section", 
+                          label = glue("Sections for {chpt}"),
+                          choices = sects)
+    })
+    
+    observe({
+        chpt <- input$chapt
+        sub <- input$section
+        charts <- get_charts(chpt, sub)
+        if(nchar(sub) > 0) {
+            updateSelectInput(session, "chart",
+                              label = glue("Charts for {sub}"),
+                              choices = charts)
+        }
+        
+    })
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
